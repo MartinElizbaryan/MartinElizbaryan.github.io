195517e4443bb9fdfcec11638e1c78d7af3ddcb6
doc <Martin.elizbaryan@gmail.com>
2018-07-06 00:03:22 +0400

    my cv

---
+++ js/high-script.js
@@ -0,0 +1,141 @@
+Math.easeOutBounce = function (pos) {
+    if ((pos) < (1 / 2.75)) {
+        return (7.5625 * pos * pos);
+    }
+    if (pos < (2 / 2.75)) {
+        return (7.5625 * (pos -= (1.5 / 2.75)) * pos + 0.75);
+    }
+    if (pos < (2.5 / 2.75)) {
+        return (7.5625 * (pos -= (2.25 / 2.75)) * pos + 0.9375);
+    }
+    return (7.5625 * (pos -= (2.625 / 2.75)) * pos + 0.984375);
+};
+
+var chart = Highcharts.chart('container', {
+
+    title: {
+        text: 'Languages',
+        style: {
+            fontWeight: 'bold',
+            fontSize: '20px'
+        }
+    },
+
+    chart: {
+        // plotBorderWidth: 1,
+        
+        // plotBorderColor: '#cccccc'
+    },
+
+    xAxis: {
+        categories: ['HTML5', 'CSS3', 'Bootstrap4', 'JavaScript', 'JQuery', 'VueJS', 'Angular1', 'PHP', 'MySQL', 'Laravel'],
+        labels: {
+            style: {
+                color: 'black',
+                fontSize: "14px",
+                fontWeight: "bold"
+            }
+        },
+        lineColor: '#707073',
+        tickColor: '#707073',
+    },
+
+    yAxis: {
+        title: {
+            text: 'Skills percent'
+        },
+        labels: {
+            formatter: function () {
+                return this.value + ' %';
+            },
+
+            style: {
+                color: 'black',
+                fontSize: "16px",
+            }
+        }
+    },
+
+    tooltip: {
+        crosshairs: true,
+        shared: true,
+        useHTML: true,
+        headerFormat: '<table class="charts_table"><tr><td><b>{point.key} : ',
+        pointFormat: '{point.y} %</b></td></tr>',
+        footerFormat: '</table>',
+        valueDecimals: 2,
+
+        backgroundColor: 'rgba(255, 255, 255, 0.7)',
+        style: {
+            // color:  
+        }
+    },
+
+
+    plotOptions: {
+        spline: {
+            marker: {
+                radius: 4,
+            }
+        },
+        column: {
+            borderRadius: 5
+        },
+        series: {
+            animation: {
+                duration: 2000,
+                easing: 'easeOutBounce'
+            },
+            borderWidth: 1,
+            borderColor: 'black'
+        }
+    },
+
+
+    colors: ['#E44D26', '#1E88DC', '#563D7C', '#63A814', '#183555',
+    '#42B883', '#B63032', '#6181B6', '#cccccc', '#FA503A'],
+
+    series: [{
+        type: 'column',
+        colorByPoint: true,
+        data: [95, 95, 75, 85, 85, 30, 30, 80, 75, 60],
+        showInLegend: false,
+    }],
+
+});
+
+$('#plain').click(function () {
+    chart.update({
+        chart: {
+            inverted: false,
+            polar: false
+        },
+    });
+    charts($(this))
+});
+
+$('#inverted').click(function () {
+    chart.update({
+        chart: {
+            inverted: true,
+            polar: false
+        },
+    });
+    charts($(this))
+});
+
+$('#polar').click(function () {
+    chart.update({
+        chart: {
+            inverted: false,
+            polar: true
+        },
+    });
+    charts($(this))
+});
+
+function charts(it){
+    $(".types_of_chart").removeClass("active")
+    $(it).addClass("active")
+}
+
--- js/profile.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$(document).keydown(function(e){
-        if(e.key== "Q"){
-           $(location).attr('href', "index.php");
-        }
-    })
-})
